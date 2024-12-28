fd43caa86ad7fe4b9831be8077abd36ec91daa7e
AnishKarchuli <aksk099@hotmail.com>
2024-12-28 10:42:27 +0530

    updated cors origin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://bi-1-3-hw-1-mocha.vercel.app/",
   credentials: true,
   optionSuccessStatus: 200,
 }
